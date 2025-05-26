cd894d94cae3c7cad4dab036578da18ea3be16a4
mAria6265 <maria.lucia.fonseca@escola.pr.gov.br>
2025-05-26 19:49:53 +0000

    sétimo commit - aula 6

---
+++ assets/perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    'História',
+    'O que foi a Guerra Fria?',
+   'Um conflito político, ideológico e econômico entre Estados Unidos e União Soviética, sem confronto militar direto, que durou de 1947 a 1991.'
+)
+criaCartao(
+    'História',
+    'Quais ideologias estavam envolvidas na Guerra Fria?',
+   'Capitalismo (liderado pelo EUA e apoiado pela OTAN) e Socialismo/Comunismo (liderado pela URSS e apoiado pelo Pacto de Varsóvia)'
+)
+criaCartao(
+    'História',
+    'O que é a OTAN e qual seu objetivo?',
+    'A OTAN, ou Organização do Tratado do Atlântico Norte, foi criada em 4 de abril de 1949, como uma aliança militar entre Estados Unidos, Canadá e dez países da Europa Ocidental. Liderado pelos EUA, e seu principal objetivo da OTAN era garantir a defesa coletiva dos seus membros: um ataque a qualquer país da aliança seria considerado um ataque contra todos.'
+)
