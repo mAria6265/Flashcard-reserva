f0c11a9b134c7340a5cf1a2933e4e18b8790a1ea
mAria6265 <maria.lucia.fonseca@escola.pr.gov.br>
2025-05-27 00:28:06 +0000

    Oitavo commit ( finalizado)

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'História - Revolução Francesa',
+    'O que foi a Revoloção Francesa',
+    'A Revolução Francesa foi um movimento social, político e econômico que ocorreu na França entre 1789 e 1799. Ela derrubou a monarquia absolutista, estabeleceu a república e promoveu ideias de liberdade, igualdade e fraternidade. '
+)
+
+criaCartao(
+    'História - Revolução Francesa',
+    'O que a causou?',
+    'A principal causa da Revolução Francesa foi a profunda desigualdade social e econômica. A sociedade era dividida em três estados: o clero e a nobreza tinham privilégios, enquanto o Terceiro Estado — composto pela maioria da população — pagava altos impostos e vivia em pobreza. E, a crise financeira do governo, causada por gastos excessivos e guerras, agravou a situação. Ideias iluministas também tiveram certa influência na maneira do povo pensar. '
+)
+
+criaCartao(
+    'Geografia - Efeito Estufa',
+    'O que é o Efeito estufa e quais as consequências de seu aumento desenfreado?',
+    'O efeito estufa é um processo natural que ocorre na atmosfera terrestre, no qual certas gases retêm o calor proveniente do sol, mantendo a temperatura do planeta adequada para a vida, mas seu aumeto desequilibrado pode ser nocivo a nós. E quando passa a ser nocivo, pode causar certas consequências, como o aumento da temperatura, derretimento das calotas polares, aumento médio do nível dos oceanos, áreas litorâneas submerssas'
+)
+
+criaCartao(
+    'Geografia - Efeito Estufa',
+    'Como nós seres humanos intensificamos o aumento do Efeito Estufa?',
+    'Com as Revoluções Industriais (que trazem muitas indústrias com energias renováveis e não renováveis, e produzem em grande escala), com a Urbanização(muitos automóveis e mobilidade humana) e Produção Agropecuária(agrotóxicos, desmatamento e criação de gado).'
+)
+
+criaCartao(
+    'Matemática - Probabilidade',
+    'O que é probabilidade?',
+    'É a medida da chance de um evento acontecer. Ela expressa a possibilidade de ocorrência de um resultado, variando de 0 (impossível) a 1 (certo). Para calcular a probabilidade, divide-se o número de casos favoráveis pelo número total de casos possíveis. Esse conceito é muito usado para prever resultados em jogos, sorteios e experimentos.'
+)
+
+criaCartao(
+    'Matemática - Análise Combinatória',
+    'Qual a definição de análise combinatória?',
+    'A análise combinatória é um ramo da matemática que estuda os métodos de contagem, permitindo calcular de quantas maneiras diferentes é possível formar grupo, organizar elementos ou fazer escolhas.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é a teoria celular e quais são seus principais postulados? Explique a importância dessa teoria para a biologia.',
+    'A teoria celular é um dos princípios fundamentais da biologia que estabelece que todos os organismos vivos são formados por células, que a célula é a unidade básica da vida e que todas as células se originam de outras células preexistentes. Essa teoria revolucionou a ciência ao unificar o entendimento dos seres vivos, mostrando que tanto organismos unicelulares quanto multicelulares dependem da célula para funções vitais, crescimento e reprodução.'
+)
+
+criaCartao(
+    'Biologia',
+    'Explique o que é e como funciona a fotossíntese.',
+    'A fotossíntese é o processo das plantas que usa a luz do sol para produzir alimento. Elas transformam água e dióxido de carbono em glicose e oxigênio. A glicose serve como energia para a planta crescer e viver. O oxigênio liberado é importante para a respiração dos seres vivos.'
+)
+
+criaCartao(
+    'Português - Poema e eu lírico',
+    'O que é o eu lírico?',
+    'Também chamado de eu poético é a voz que expressa em um poema. Essa voz manifesta sentimentos, emoções, pensamentos e opiniões. Usado principalmente em poesia, especialmente em poemas líricos, para transmitir a visão pessoal do poeta. Você usa o eu lírico quando quer mostrar emoções íntimas, reflexões pessoais ou uma experiência vivida, diferente de uma narrativa objetiva. '
+)
+
+criaCartao(
+    'Português - Poesia',
+    'Quais as características da poesia romântica.',
+    'A poesia romântica valoriza muito os sentimentos, como o amor e a saudade, e vê a natureza como fonte de inspiração e refúgio. O amor costuma ser idealizado e, às vezes, trágico. Há também um forte sentimento nacionalista e valorização da cultura do país. Os poetas expressam suas emoções de forma livre e subjetiva, fugindo da realidade.'
+)
+
+criaCartao(
+    'Inglês',
+    'Explique a diferença entre o uso do Present Simple e do Present Continuous em inglês, dando exemplos.',
+    'O Present Simple é usado para falar de hábitos, fatos gerais e verdades universais, como em "She works every day." Já o Present Continuous indica ações que estão acontecendo no momento da fala ou planos futuros, como em "She is working right now."'
+)
+
+criaCartao(
+    'Inglês',
+    ' Explique a estrutura de uma frase no Past Simple e quando ela é usada.',
+    'O Past Simple é usado para falar de ações concluídas no passado. A estrutura é sujeito + verbo no passado (regular com -ed ou forma irregular). Exemplo: “They visited the museum yesterday.”'
+)
